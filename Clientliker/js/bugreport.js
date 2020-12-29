83a1b18dd38b9dc11cae0594109fb9dd7ceb13c2
sudo-aptinstaller <leninben18@gmail.com>
2020-12-29 20:34:57 +0530

    final_1.04

--- Clientliker/js/bugreport.js
+++ Clientliker/js/bugreport.js
@@ -9,13 +9,13 @@ $(document).ready(function (e) {
   //add fs get file here 
   fs.readFile(remote.app.getPath('userData')+'/applicationData/user.joel', (error,data) =>{
     if(!error){
-
       appId = data;
     }
   });
       $('#reportForm').on('submit',(function(e) {
           e.preventDefault();
           var formData = new FormData(this);
+          $('#reportForm').hide();
           console.log(formData);
           $.ajax({
               type:'POST',
@@ -26,6 +26,8 @@ $(document).ready(function (e) {
               processData: false,
               success:function(response){
                   console.log(response);
+                  $('#reportForm').html('<h3 class="nerko">Thank you for the feedback. </h3>');
+                  $('#reportFomr').show();
               },
               error: function(data){
                   console.log('Error : ',data);
