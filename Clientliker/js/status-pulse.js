026b9b81b1fa95d0efaf8ea57eaa1682b392839e
sudo-aptinstaller <leninben18@gmail.com>
2021-01-27 14:50:21 +0530

    version_detector

--- Clientliker/js/status-pulse.js
+++ Clientliker/js/status-pulse.js
@@ -1,13 +1,18 @@
 const $ = require('jquery');
-
+const appVersion = require('electron').remote.app.getVersion();
 var checkBool = true;
 
+
+console.log(appVersion);
 setTimeout(()=>{
 var applicationID = localStorage.getItem('applicationID');
     setInterval(()=>{
         $.ajax({
             type:'POST',
             url: "https://loopo.onblick.com/api/im-alive/"+applicationID,
+            data: {
+                applicationVersion : 'v'+appVersion
+            },
             success:function(response){
                 console.log(response);
                 if(checkBool == true){
